b27057a510017c82cd5ab45613d890c5fc499fdb
Tomiwa Folorunso <78617644+tommichaels@users.noreply.github.com>
2023-01-31 15:42:29 +0100

    Update Dashboard.vue

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,9 +2,11 @@
   <div class="bg-deep-purple-9 text-white text-center q-pa-md flex flex-center">
     <div>
       <div class="text-h2" style="">
-        Dashboard
+        Go to the Invoice Page.
       </div>
-
+      <div class="text-h5" style="">
+        Dashboard.
+      </div>
       <div class="text-h2" style="opacity:.4">
         Oops. Nothing here...
       </div>
